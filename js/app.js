4d6b0cf18833ee0c7d7916ddaa0840f6a388f6e2
Codingmasterfred <fredrickg777@gmail.com>
2023-02-28 09:56:52 -0800

    Q4

--- js/app.js
+++ js/app.js
@@ -95,7 +95,7 @@ function sumArray(sumArr) { //eslint-disable-line
     
 
 } console.log(sumArray(testArray))
-//sumArray(testArray);
+// sumArray(testArray);
 // let sum = 0;
     // for(let i = 0; i < sumArr.length; i++){
     //     sum( sumArr[i]);
@@ -114,7 +114,7 @@ function sumArray(sumArr) { //eslint-disable-line
  
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
